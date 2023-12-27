97ed84fbeaca60cc22542adfcb7dcd0fff7d4028
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-12-27 00:30:22 +0000

    solve with brute force approach and write 2nd approach idea

---
+++ question_1/script.js
@@ -0,0 +1,45 @@
+/**
+ * Given 2 arrays, create a function that
+ * let's a user know (true / false) whether
+ * these two arrays contain any common items
+ * 
+ * For Example:
+ * const array1 = ["a", "b", "c", "x"];
+ * const array2 = ["z", "y", "i"];
+ * should return false.
+ * 
+ * -----------------------------------------
+ * 
+ * const array1 = ["a", "b", "c", "x"];
+ * const array2 = ["z", "y", "x"];
+ * should return true.
+ */
+
+// 2 parameters - arrays - no size limit
+// return true or false (boolean)
+
+const array1 = ["a", "b", "c", "x"];
+const array2 = ["z", "y", "x"];
+
+/** Brute-Force Approach */
+function containsCommonItem(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                return true;
+            }
+        }
+    }
+    
+    return false;
+}
+
+// Time Complexity: O(a * b)
+
+/** 2nd Approach */
+function containsCommonItem2(arr1, arr2) {
+    // Loop through first array and create object where properties === items in the array
+    // Loop through second array and check if item in second array exists on created object
+}
+
+// Time Complexity: O(a + b)
