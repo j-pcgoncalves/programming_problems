d941485b4a4061e5d634d27ea67a3ba46888cc4a
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-12-27 11:01:52 +0000

    create 2nd approach

--- question_1/script.js
+++ question_1/script.js
@@ -39,7 +39,26 @@ function containsCommonItem(arr1, arr2) {
 /** 2nd Approach */
 function containsCommonItem2(arr1, arr2) {
     // Loop through first array and create object where properties === items in the array
+    let map = {};
+
+    for (let i = 0; i < arr1.length; i++) {
+        const item = arr1[i];
+
+        if (!map[item]) {
+            map[item] = true;
+        }
+    }
+
     // Loop through second array and check if item in second array exists on created object
+    for (let j = 0; j < arr2.length; j++) {
+        const item = arr2[j];
+
+        if (map[item]) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 // Time Complexity: O(a + b)
