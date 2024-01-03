f0d1107f914280b4b1a26a7e55c78569073eaeb1
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-01-03 23:51:33 +0000

    refactor question

--- question_3/script.js
+++ question_3/script.js
@@ -1,13 +1,15 @@
-// Create a function that reverses a string:
-// "Hi. My name is Andrei" should be:
-// "ierdnA si eman yM .iH"
+/**
+ * Create a function that reverses a string:
+ * "Hi. My name is Andrei" should be:
+ * "ierdnA si eman yM .iH"
+ */
 
 function reverse(str) {
     // Check Input
     if (!str || str.length < 2 || typeof str !== "string") {
         return "Something is not right with your input.";
     }
-    
+
     const strArr = str.split("");
     strArr.reverse();
 
