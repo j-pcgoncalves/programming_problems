7a51b2bc39e955bf83fb20d36d4a35cd96d8cf6f
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-01-03 23:44:05 +0000

    add input checking

--- question_3/script.js
+++ question_3/script.js
@@ -3,6 +3,11 @@
 // "ierdnA si eman yM .iH"
 
 function reverse(str) {
+    // Check Input
+    if (!str || str.length < 2 || typeof str !== "string") {
+        return "Something is not right with your input.";
+    }
+    
     const strArr = str.split("");
     strArr.reverse();
 
