a86b4fd78255e17dad26b2ac01bb27c4fa002754
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-06-12 20:47:22 +0100

    create keys method

--- hash_tables/build_hash_table/script.js
+++ hash_tables/build_hash_table/script.js
@@ -38,8 +38,22 @@ class HashTable {
 
         return undefined;
     }
+
+    keys() {
+        const keysArray = [];
+
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i]) {
+                keysArray.push(this.data[i][0][0]);
+            }
+        }
+
+        return keysArray;
+    }
 };
 
 const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 10000);
-myHashTable.get("grapes");
+myHashTable.set("apples", 54);
+myHashTable.set("oranges", 2);
+console.log(myHashTable.keys());
